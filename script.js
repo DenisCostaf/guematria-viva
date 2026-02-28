d8b592c8be0ba24a5e76ad2a7b76a7d0adeae98e
DenisCostaf <ferreiradenis98@gmail.com>
2026-02-28 18:21:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-<script src="script.js"></script>
+const valores = {
+  a: 1, b: 2, c: 3, d: 4, e: 5,
+  f: 6, g: 7, h: 8, i: 9,
+  j: 10, k: 20, l: 30, m: 40,
+  n: 50, o: 60, p: 70, q: 80,
+  r: 90, s: 100, t: 200,
+  u: 300, v: 400, w: 500,
+  x: 600, y: 700, z: 800
+};
+
+function calcularGuematria() {
+  const nome = document.getElementById("nomeInput").value.toLowerCase();
+  let soma = 0;
+
+  for (let letra of nome) {
+    if (valores[letra]) {
+      soma += valores[letra];
+    }
+  }
+
+  document.getElementById("resultado").innerText =
+    "Valor da Guematria: " + soma;
+}
